23651b69f59a8539b74b82eb830e9d77d8069765
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-11-14 14:25:51 -0300

    feat - implement footer with tailwind style

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+
+function Footer() {
+  return (
+    <footer className="flex justify-center p-1 fixed bottom-0 text-center w-full bg-gray-800">
+        <p className="text-white">Feito com ❤️ por <a href="https://elielsondev.vercel.app/" target="_blank">Elielson Nascimento</a></p>
+    </footer>
+  )
+}
+
+export default Footer;
