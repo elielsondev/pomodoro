65f41bfa4ae38564702ba0aa5b3e4f8232054f7e
Elielson Nascimento <nascimento.elielson@gmail.com>
2024-11-14 13:54:33 -0300

    feat - tailwind implementation

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="flex justify-center items-center p-2 shadow-lg bg-green-700">
+        <h1 className="text-4xl animate-bounce text-yellow-500">Pomodoro</h1>
+    </header>
+  )
+}
+
+export default Header;
